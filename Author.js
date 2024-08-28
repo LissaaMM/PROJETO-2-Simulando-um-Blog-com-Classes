cb095c861cdc358f53e9b007c16154db486361eb
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2024-08-28 08:21:20 -0300

    Update Author.js

--- Author.js
+++ Author.js
@@ -3,13 +3,13 @@ const Post = require('./Post')
 class Author {
   constructor(name) {
     this.name = name
-    this.publi = []
+    this.post = []
   }
 
   createPost(title, content) {
-    const publi = new Post(title, content, this)
-    this.publi.push(publi)
-    return publi
+    const post = new Post(title, content, this)
+    this.post.push(post)
+    return post
   }
 }
 
